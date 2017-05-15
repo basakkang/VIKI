8a6129de335fb7a1d0f4ddbc6910cdea66c43343
Changsung Kim <cstiger99@gmail.com>
2017-05-15 15:29:22 +0900

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ cerebro = DQNCerebro(20,20)
 cerebro.setInstruments(["000660.KS", "070960.KS"])
 
 start_l = datetime.datetime(2006, 12, 1)
-end_l = datetime.datetime(2016, 6, 31)
+end_l = datetime.datetime(2016, 6, 30)
 cerebro.setLearningDate(start_l, end_l)
 start_t = datetime.datetime(2016, 7, 1)
 end_t = datetime.datetime(2017, 5, 14)
