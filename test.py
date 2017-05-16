d5808f43da89981d550809d073fca48c6cbe731e
Changsung Kim <cstiger99@gmail.com>
2017-05-16 12:10:13 +0900

    update average rate

--- test.py
+++ test.py
@@ -31,8 +31,6 @@ cerebro.buildModel()
 labels, codes, s_date, end_date, date_list = cerebro.getTestingResult()
 print labels
 
-feed = Feed(s_date, end_date)
-
 df_total = None
 
 df_list = []
@@ -53,16 +51,44 @@ for idx, item in enumerate(df_total['Volume'].values.tolist()):
 		idx_list.append(idx)
 df_total = df_total.drop(df_total.index[idx_list]).ix[21::]
 
+def getRiseRate(df, codes):
+	rate_dict = {}
+	for code in codes:
+		prices_code = map(lambda x:x[0], df[[strCode(code)]].values.tolist())
+		rate_dict[code] = float(prices_code[-1] - prices_code[0])/float(prices_code[0]) * 100
+	return rate_dict
+
+def getKospiRate(df):
+	ind_list = df.index.tolist()
+	df2 = web.DataReader("^KS11", 'yahoo', s_date, end_date)[['Close']]
+	kospi = map(lambda x:x[0], df2.values.tolist())
+	return float(kospi[-1] - kospi[0])/float(kospi[0]) * 100
+
+rise_dict = getRiseRate(df_total, codes)
+kosp_rate = getKospiRate(df_total)
+
+sum_val = 0
+for key, val in rise_dict.iteritems():
+	sum_val += val
+
+
+
+print "average rate : " + str(float(sum_val)/len(codes))
+print "kospi rate : " + str(kosp_rate)
 # df_total = df + result 
 df_dict = {}
 for idx, code in enumerate(codes):
 	df_dict[code] = df_total[[strCode(code)]]
 	df_dict[code].columns = ['Close']
 
+feed = Feed(s_date, end_date)
+
 for key, val in df_dict.iteritems():
 	feed.addDailyFeed(val, key)
 
 # # add all daily feed
+
+
 alphaman = Alphaman(s_date, end_date)
 alphaman.setFeed(feed)
 alphaman.setStrategy(VikiStrategy(codes, labels))
